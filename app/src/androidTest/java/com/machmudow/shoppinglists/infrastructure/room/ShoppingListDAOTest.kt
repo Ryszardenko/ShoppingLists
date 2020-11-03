ac0b06849d1a5c1737701e7f873ef47a8f8a91fd
machmudow <ryszard.machmudow@forum-media.pl>
2020-11-03 18:18:36 +0100

    Add Room tests

---
+++ app/src/androidTest/java/com/machmudow/shoppinglists/infrastructure/room/ShoppingListDAOTest.kt
@@ -0,0 +1,85 @@
+package com.machmudow.shoppinglists.infrastructure.room
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
+import com.machmudow.shoppinglists.getOrAwaitValue
+import com.machmudow.shoppinglists.infrastructure.model.ShoppingList
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class ShoppingListDAOTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var database: AppDatabase
+    private lateinit var dao: ShoppingListDAO
+
+    @Before
+    fun setup() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            AppDatabase::class.java
+        ).allowMainThreadQueries().build()
+        dao = database.getShoppingListDAO()
+    }
+
+    @After
+    fun tearDown() {
+        database.close()
+    }
+
+    @Test
+    fun insertShoppingList() {
+        val shoppingList = ShoppingList(title = "Test")
+        dao.insertShoppingList(shoppingList)
+
+        val shoppingListWithItems = dao.getShoppingListsWithItems().getOrAwaitValue()
+
+        assertThat(shoppingListWithItems.map { it.shoppingList }.contains(shoppingList))
+    }
+
+    @Test
+    fun delete() {
+        val shoppingList = ShoppingList(title = "Test")
+        dao.insertShoppingList(shoppingList)
+        dao.delete(shoppingList.id)
+
+        val shoppingListWithItems  = dao.getShoppingListsWithItems().getOrAwaitValue()
+
+        assertThat(!shoppingListWithItems.map { it.shoppingList }.contains(shoppingList))
+    }
+
+    @Test
+    fun archive() {
+        val shoppingList = ShoppingList(title = "Test")
+        dao.insertShoppingList(shoppingList)
+
+        dao.archive(shoppingList.id)
+
+        val shoppingListWithItems  = dao.getShoppingListsWithItems().getOrAwaitValue()
+
+        assertThat(!shoppingListWithItems.map { it.shoppingList }.contains(shoppingList))
+    }
+
+    @Test
+    fun unarchive() {
+        val shoppingList = ShoppingList(title = "Test", isArchived = true)
+        dao.insertShoppingList(shoppingList)
+
+        dao.unarchive(shoppingList.id)
+
+        val shoppingListWithItems  = dao.getShoppingListsWithItems().getOrAwaitValue()
+
+        assertThat(!shoppingListWithItems.map { it.shoppingList }.contains(shoppingList))
+    }
+}
